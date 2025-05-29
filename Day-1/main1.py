1a93a05581525067feb19db21a38d0f9d05b961c
Kingsley Ofoegbu <kingsley_ofoegbu@yahoo.com>
2025-05-29 23:24:56 +0000

    Brand Name Generator

---
+++ Day-1/main1.py
@@ -0,0 +1,9 @@
+print("We want to make a brand name for you")
+
+city = input("What is your city name?\n")
+pet = input("What is your pet name?\n")
+print("Thank you for using our brand name generator!")
+print("Have a great day!")
+print("Your brand name is ready!")
+print("We hope you like your brand name!")
+print("Your brand name is " + city + " " + pet)
